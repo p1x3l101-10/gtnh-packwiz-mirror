3926430893d6bb510efb1f7395f94611f3597767
Pixel <scott.blatt.0b10@gmail.com>
2025-10-09 15:55:18 -0500

    add build files

---
+++ README.md
@@ -0,0 +1,5 @@
+This repo is mostly just serves as a file host
+
+The outputs are automatically generated under `./pack`
+
+The tool that generates them is [https://github.com/p1x3l101-10/gtnh-2-packwiz](https://github.com/p1x3l101-10/gtnh-2-packwiz)
---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+PACK_VERSION="2.8.0"
+
+if [[ -e ./build ]]; then
+  rm -rf ./build
+fi
+
+git clone --recursive https://github.com/p1x3l101-10/gtnh-2-packwiz build
+
+pushd ./build
+cmake --workflow . --preset dev
+popd
+
+exec ./build/build/gtnh-2-packwiz --pack-version="${PACK_VERSION}" --config=./config.toml "$@"
